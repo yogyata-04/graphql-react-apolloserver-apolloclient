4da68bd6a7b13bfe0b512c77b8232882dad86e9d
yogyata <yogyatasingh2002@gmail.com>
2026-03-28 02:08:47 +0530

    create query and resolvers for server

---
+++ fakedb.js
@@ -0,0 +1,47 @@
+// concept of collection and documents
+export const users = [
+  {
+    id: "1",
+    name: "John Doe",
+    email: "john@gmail.com",
+    password: "password123"
+  },
+  {
+    id: "2",
+    name: "Jane Smith",
+    email: "jane@gmail.com",
+    password: "password456"
+  }
+];
+
+export const posts = [
+    {
+        id: "1",
+        title: "Sunset",
+        description: "A beautiful sunset over the mountains.",
+        url: "https://example.com/sunset.jpg",
+        userId: "1"
+    },
+    {
+        id: "2",
+        title: "Beach",
+        description: "A sunny day at the beach.",
+        url: "https://example.com/beach.jpg",
+        userId: "2"
+    },
+    {
+        id: "3",
+        title: "Cityscape",
+        description: "A bustling city at night.",
+        url: "https://example.com/cityscape.jpg",
+        userId: "1"
+    },
+    {
+        id: "4",
+        title: "Forest",
+        description: "A serene forest with tall trees.",
+        url: "https://example.com/forest.jpg",
+        userId: "2"
+    }
+
+]
--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 import {ApolloServer,gql} from 'apollo-server';
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+import {users, posts} from './fakedb.js'
 
 const typeDefs = gql`
     type Query {
-        greet:String
+        users: [User!]!
+        posts: [Post!]!
+        user(id: ID!):User
+        post(id: ID!): [Post!]!
+    }
+    type User {     
+        id: ID!
+        name: String!
+        email: String!
+        password: String!
+        posts: [Post!]!
+    }
+    type Post {
+        id: ID!
+        title: String!
+        description: String!
+        url: String!
+        userId: ID!
     }
 `
 const resolvers={
     Query:{
-        greet:()=>"Hello World"
+        users:()=>users,
+        posts:()=>posts,
+        // user:(_,args)=>{users.find(user=>user.id==args.id)}, here _, is parent and as parent here is undefined we simply put _
+        user:(_,{id})=>users.find(user=>user.id==id),  //destructuring the args to get id directly
+        post:(_,{id})=>posts.filter(post=>post.userId==id), //here we need filter instead of find because we have one to many mapping between user and posts, so we need to return an array of posts for a user
+    },
+    User:{
+        posts:(parent)=>posts.filter(post=>post.userId==parent.id)
     }
 }
 
